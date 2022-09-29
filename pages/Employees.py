a50341b28139d3c41614b35123fe2863dba9c2d5
Arjune Krishna <78844048+arjune-krishna@users.noreply.github.com>
2022-09-29 23:01:55 +0530

    Update Employees.py

--- pages/Employees.py
+++ pages/Employees.py
@@ -24,6 +24,9 @@ st.set_page_config(page_title='Employees', page_icon='👥', layout='wide')
 
 st.title('👥Employees')
 
+
+
+
 #Salaries of Active Employees   
 salaries_active_employees = salaries[salaries['to_date'].astype(str).str.contains('9999')]
 
@@ -31,77 +34,84 @@ salaries_active_employees = salaries[salaries['to_date'].astype(str).str.contain
 dept_employees = dept_employees.merge(departments, left_on='dept_no', right_on='dept_no')
 dept_manager= dept_manager.merge(departments, left_on='dept_no', right_on='dept_no')
 
-
-col1, col2, col3 = st.columns(3)
-with col1:
+try:
     selected_employee = st.number_input("Select the Employee:", 10001, 499999)
 
-#Work History
-work_history = dept_employees
-work_history = work_history[work_history['emp_no'] == selected_employee]
-work_history['to_date'] = work_history['to_date'].astype(str).str.replace('01-01-9999', '')
-work_history['from_date'] = pd.to_datetime(work_history['from_date'], format="%d-%m-%Y", infer_datetime_format=True)
-work_history['to_date'] = work_history['to_date'].str.split('-').apply(clean_row)
-work_history = work_history.sort_values(by='from_date')
-work_history["from_date"] = work_history["from_date"] .dt.strftime("%Y-%m-%d")
-work_history_output = work_history[['dept_name', 'from_date',	'to_date']].set_axis(["Department Name", "From", "To"], axis=1).reset_index().drop(['index'], axis=1)
+    col1, col2, col3 = st.columns(3)
+    with col1:
+    
+        #Work History
+        work_history = dept_employees
+        work_history = work_history[work_history['emp_no'] == selected_employee]
+        work_history['to_date'] = work_history['to_date'].astype(str).str.replace('01-01-9999', '')
+        work_history['from_date'] = pd.to_datetime(work_history['from_date'], format="%d-%m-%Y", infer_datetime_format=True)
+        work_history['to_date'] = work_history['to_date'].str.split('-').apply(clean_row)
+        work_history = work_history.sort_values(by='from_date')
+        work_history["from_date"] = work_history["from_date"] .dt.strftime("%Y-%m-%d")
+        work_history_output = work_history[['dept_name', 'from_date',	'to_date']].set_axis(["Department Name", "From", "To"], axis=1).reset_index().drop(['index'], axis=1)
 
 #Salary Graph
 
-work_salary = salaries[salaries['emp_no'] == selected_employee]
-work_salary['to_date'] = work_salary['to_date'].astype(str).str.replace('01-01-9999', '')
-work_salary['from_date'] = pd.to_datetime(work_salary['from_date'], infer_datetime_format=True)
-work_salary['to_date'] = work_salary['to_date'].str.split('-').apply(clean_row)
-work_salary['from_date'] = work_salary['from_date'].rank()
+        work_salary = salaries[salaries['emp_no'] == selected_employee]
+        work_salary['to_date'] = work_salary['to_date'].astype(str).str.replace('01-01-9999', '')
+        work_salary['from_date'] = pd.to_datetime(work_salary['from_date'], infer_datetime_format=True)
+        work_salary['to_date'] = work_salary['to_date'].str.split('-').apply(clean_row)
+        work_salary['from_date'] = work_salary['from_date'].rank()
 
 
 
 #KPIs
 #FullName
-column1, column2, column3, column4, column5 = st.columns(5)
-with column1:
-  first_name = employees[employees['emp_no'] == selected_employee].first_name.iloc[-1]
-  last_name = employees[employees['emp_no'] == selected_employee].last_name.iloc[-1]
-  st.metric(label="📛 Name", value=f"{first_name} {last_name}")
+    column1, column2, column3, column4, column5 = st.columns(5)
+    with column1:
+        first_name = employees[employees['emp_no'] == selected_employee].first_name.iloc[-1]
+        last_name = employees[employees['emp_no'] == selected_employee].last_name.iloc[-1]
+        st.metric(label="📛 Name", value=f"{first_name} {last_name}")
 
-with column2:
-    emp_gender = employees[employees['emp_no'] == selected_employee].gender.iloc[-1]
-    if emp_gender == 'F':
-        st.metric(label="👩🏻Gender", value="Female")
-    else:
-        st.metric(label="🧑🏻Gender", value="Male")
+    with column2:
+        emp_gender = employees[employees['emp_no'] == selected_employee].gender.iloc[-1]
+        if emp_gender == 'F':
+            st.metric(label="👩🏻Gender", value="Female")
+        else:
+            st.metric(label="🧑🏻Gender", value="Male")
 
-with column3:
+    with column3:
     #Department
-    working_department = work_history.reset_index().drop(['index'], axis=1).iloc[-1].dept_name
-    st.metric(label="💻Department", value=f"{working_department}")
+        working_department = work_history.reset_index().drop(['index'], axis=1).iloc[-1].dept_name
+        st.metric(label="💻Department", value=f"{working_department}")
 
-with column4:
+    with column4:
     #Date of Birth
-    dob = employees[employees['emp_no'] == selected_employee]['birth_date'].iloc[-1]
-    st.metric(label="👶Date of Birth", value=f"{dob}")
+        dob = employees[employees['emp_no'] == selected_employee]['birth_date'].iloc[-1]
+        st.metric(label="👶Date of Birth", value=f"{dob}")
 
-with column5:
-    try:
-        current_salary = work_salary.iloc[-1].salary
-        st.metric(label="💵 Current Salary", value=f"${current_salary}")
-    except:
-        st.metric(label="💵 Current Salary", value=f"Salary Unavailable")
+    with column5:
+    #Current Salary
+       try:
+            current_salary = work_salary.iloc[-1].salary
+            st.metric(label="💵 Current Salary", value=f"${current_salary}")
+       except:
+           st.metric(label="💵 Current Salary", value=f"Salary Unavailable")
 
 
 
 
-if work_salary.empty:
-    print('Sorry! No Salary records found')
-else:
-    work_salary[['salary', 'from_date', 'to_date']] \
-    .set_axis(['Salary', 'From', 'To'], axis=1)
+    st.caption("Employees number range between 10001 and 499999")
 
-    fig = go.Figure([go.Scatter(x=work_salary['from_date'], y=work_salary['salary'], mode='lines+markers', marker={'color': 'rgb(255, 51, 51)'}, 
+
+
+
+    if work_salary.empty:
+        st.write('Sorry! No Salary records found')
+    else:
+        work_salary[['salary', 'from_date', 'to_date']] \
+            .set_axis(['Salary', 'From', 'To'], axis=1)
+
+        fig = go.Figure([go.Scatter(x=work_salary['from_date'], y=work_salary['salary'], mode='lines+markers', marker={'color': 'rgb(255, 51, 51)'}, 
         name='', hovertemplate='<br>Year: %{x}<br>Salary: %{y}<br>')])
 
 
-    fig.update_layout(
+        fig.update_layout(
         title = "Salary Chart",
         xaxis=dict(title='Year', showgrid =False),
         yaxis=dict(title='Salary', showgrid =False),
@@ -110,7 +120,10 @@ else:
         paper_bgcolor="#00172B",
         plot_bgcolor="#00172B")
 
-st.plotly_chart(fig, use_container_width=True)
+        st.plotly_chart(fig, use_container_width=True)
 
-st.markdown("Employee's Work History")
-st.table(work_history_output)
+        st.markdown("Employee's Work History")
+        st.table(work_history_output)
+except ValueError:
+    st.error("Please enter a valid employee number")
+  
